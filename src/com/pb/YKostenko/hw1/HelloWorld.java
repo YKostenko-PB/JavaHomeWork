31bcd0ea6239976814684f9146eb86a21b762451
YKostenko <jaroslav.kostenko@privatbank.ua>
2021-10-09 23:11:35 +0300

    git init

--- src/com/pb/YKostenko/hw1/HelloWorld.java
+++ src/com/pb/YKostenko/hw1/HelloWorld.java
@@ -4,6 +4,6 @@ import com.sun.org.apache.xerces.internal.impl.xs.models.XSCMUniOp;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello1 World");
+        System.out.println("Hello World");
     }
 }
