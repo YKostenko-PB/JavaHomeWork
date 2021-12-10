85bd41a88d50a8d3ed5fc59b3ed92226e012e7f2
YKostenko <jaroslav.kostenko@privatbank.ua>
2021-12-10 12:02:07 +0200

    git init

---
+++ src/com/pb/YKostenko/hw11/PhoneBook.java
@@ -0,0 +1,14 @@
+package com.pb.YKostenko.hw11;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class PhoneBook {
+    private String name;
+    private String phone;
+    private String address;
+    private LocalDate dateOfBirth;
+    private LocalDateTime timeOfChange;
+
+
+}
